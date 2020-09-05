91a7c08ea63497ee533fb29940a57b0a29c952b8
Mihir Jayavant <mihir.jayavant2@gmail.com>
2020-09-05 22:36:17 +0530

    Add project files.

---
+++ Core/Transaction/Transaction.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+
+namespace Core.Transaction
+{
+    public sealed class Transaction
+    {
+        public int Id { get; }
+        public string Title { get; }
+        public int Amount { get; }
+        public DateTime Date { get; }
+
+        public Transaction(int id, string title, int amount, DateTime date)
+        {
+            Id = id;
+            Title = title;
+            Amount = amount;
+            Date = date;
+        }
+
+        public override bool Equals(object? obj)
+            => obj switch
+            {
+                Transaction t => this == t,
+                _ => false
+            };
+
+        public override int GetHashCode()
+            => Id ^ Title.GetHashCode() ^ Amount ^ Date.GetHashCode();
+
+        public static bool operator ==(Transaction t1, Transaction t2)
+            => (t1.Id, t1.Title, t1.Amount, t1.Date) == (t2.Id, t2.Title, t2.Amount, t2.Date);
+
+        public static bool operator !=(Transaction t1, Transaction t2)
+            => !(t1 == t2);
+    }
+}
---
+++ Core/User/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Core.User
+{
+    public sealed class User
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public bool IsDefault { get; }
+        public DateTime Created { get; }
+
+        public User(int id, string name, bool isDefault, DateTime created)
+        {
+            Id = id;
+            Name = name;
+            IsDefault = isDefault;
+            Created = created;
+        }
+    }
+}
