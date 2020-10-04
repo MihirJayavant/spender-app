c469506c843895e8ae836c8a9cd1f0de48eb8633
Mihir Jayavant <mihir.jayavant2@gmail.com>
2020-10-04 13:42:11 +0530

    xamarin added

---
+++ Source/Spender/Spender/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Spender.ViewModels;
+using Spender.Views;
+using Xamarin.Forms;
+
+namespace Spender
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
